8dd0f2cba71ae95b8caf27a7164622a5a0f3ba27
Laptop\Calvin <99045362@mydavinci.nl>
2019-03-22 09:19:34 +0100

    Add Resistance

---
+++ Resistance.php
@@ -0,0 +1,11 @@
+<?php
+
+class Resistance extends Pokemon {
+
+	protected $pikachuResistanceType = "Fighting";
+	protected $pikachuResistanceValue = 20;
+
+	protected $charmeleonResistanceType = "Lightning";
+	protected $charmeleonResistanceValue = 10;
+
+}
