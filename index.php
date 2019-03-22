39ef73c24f20cbd35ddd68ded303bb2a24fe286b
Laptop\Calvin <99045362@mydavinci.nl>
2019-03-22 09:21:11 +0100

    Made a autoload register for including the classes and echo every information about the Pokemon

--- index.php
+++ index.php
@@ -6,21 +6,30 @@
 </head>
 <body>
 <?php
-	require 'Pokemon.php';
-	require 'EnergyType.php';
-	require 'Pikachu.php';
-	require 'Charmeleon.php';
+	spl_autoload_register(function ($class_name) {
+	    include $class_name . '.php';
+	});
 
 	// Pikachu //
 	$pikachuName = new Pikachu();
 	$pikachuType = new EnergyType();
 	$pikachuHitpoints = new Pikachu();
 	$pikachuHealth = new Pikachu();
+	$pikachuWeakness = new Weakness();
+	$pikachuMultiplier =  new Weakness();
+	$pikachuNameAttackER = new Attack();
+	$pikachuNameAttackPP = new Attack();
+	$pikachuDamageAttackER = new Attack();
+	$pikachuDamageAttackPP = new Attack();
 
-	echo "Name: " . $pikachuName->getPickachuName() . "<br>";
-	echo "Type: " . $pikachuType->getPickachuType() . "<br>";
-	echo "Hitpoints: " . $pikachuHitpoints->getPickachuHitpoints() . "<br>";
-	echo "Health: " . $pikachuHealth->getPickachuHitpoints() . "<br>";
+	echo "Name: " . $pikachuName->getPikachuName() . "<br>";
+	echo "Type: " . $pikachuType->getPikachuType() . "<br>";
+	echo "Hitpoints: " . $pikachuHitpoints->getPikachuHitpoints() . "<br>";
+	echo "Health: " . $pikachuHealth->getPikachuHealth() . "<br>";
+	echo "Weakness type: " . $pikachuWeakness->getPikachuWeakness() . "<br>";
+	echo "Multiplier: " . $pikachuMultiplier->getPikachuMultiplier() . "<br>";
+	echo "Attack 1: " . $pikachuNameAttackER->getPikachuNameAttackER() . "<br>" . "Damage: " . $pikachuDamageAttackER->getPikachuDamageER() . "<br>";
+	echo "Attack 2: " . $pikachuNameAttackPP->getPikachuNameAttackPP() . "<br>" . "Damage: " . $pikachuDamageAttackPP->getPikachuDamagePP() . "<br>";
 
 	// Breakpoint //
 	echo "<br>";
@@ -30,11 +39,21 @@
 	$charmeleonType = new EnergyType();
 	$charmeleonHitpoints = new Charmeleon();
 	$charmeleonHealth = new Charmeleon();
+	$charmeleonWeakness = new Weakness();
+	$charmeleonMultiplier =  new Weakness();
+	$charmeleonNameAttackHB = new Attack();
+	$charmeleonNameAttackF = new Attack();
+	$charmeleonDamageAttackHB = new Attack();
+	$charmeleonDamageAttackF = new Attack();
 
 	echo "Name: " . $charmeleonName->getCharmeleonName() . "<br>";
 	echo "Type: " . $charmeleonType->getCharmeleonType() . "<br>";
 	echo "Hitpoints: " . $charmeleonHitpoints->getCharmeleonHitpoints() . "<br>";
-	echo "Health: " . $charmeleonHealth->getCharmeleonHitpoints() . "<br>";
+	echo "Health: " . $charmeleonHealth->getCharmeleonHealth() . "<br>";
+	echo "Weakness type: " . $charmeleonWeakness->getCharmeleonWeakness() . "<br>";
+	echo "Multiplier: " . $charmeleonMultiplier->getCharmeleonMultiplier() . "<br>";
+	echo "Attack 1: " . $charmeleonNameAttackHB->getCharmeleonNameAttackHB() . "<br>" . "Damage: " . $charmeleonDamageAttackHB->getCharmeleonDamageHB() . "<br>";
+	echo "Attack 2: " . $charmeleonNameAttackF->getCharmeleonNameAttackF() . "<br>" . "Damage: " . $charmeleonDamageAttackF->getCharmeleonDamageF() . "<br>";
 ?>
 </body>
 </html>
