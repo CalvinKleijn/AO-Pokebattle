270892ae670f3a0e7931dd74768e6ad2a32d946f
Laptop\Calvin <99045362@mydavinci.nl>
2019-03-22 09:19:25 +0100

    Add attack

---
+++ Attack.php
@@ -0,0 +1,15 @@
+<?php 
+
+class Attack extends Pokemon {
+
+	protected $pikachuNameAttackER = "Electric Ring";
+	protected $pikachuNameAttackPP = "Pika Punch";
+	protected $pikachuDamageAttackER = 50;
+	protected $pikachuDamageAttackPP = 20;
+
+	protected $charmeleonNameAttackHB = "Head Butt";
+	protected $charmeleonNameAttackF = "Flare";
+	protected $charmeleonDamageAttackHB = 10;
+	protected $charmeleonDamageAttackF = 30;
+
+}
