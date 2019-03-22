f31115b496359f3ef9790ed5f1de74dcb807ecde
Laptop\Calvin <99045362@mydavinci.nl>
2019-03-22 09:19:43 +0100

    Add Weakness

---
+++ Weakness.php
@@ -0,0 +1,11 @@
+<?php
+
+class Weakness extends Pokemon {
+
+	protected $pikachuWeaknessType = "Fire";
+	protected $pikachuMultiplier = 1.5;
+
+	protected $charmeleonWeaknessType = "Water";
+	protected $charmeleonMultiplier = 2;
+
+}
