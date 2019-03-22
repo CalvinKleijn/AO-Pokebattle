d04cefe10c1bb4dd45b229a13e6ac48fdf9c188b
Laptop\Calvin <99045362@mydavinci.nl>
2019-03-22 09:20:04 +0100

    Made functions for each class

--- Pokemon.php
+++ Pokemon.php
@@ -2,20 +2,44 @@
 
 class Pokemon {
 
-	public function __construct() {
-		// Test
-	}
-
-	public function getPickachuName() {
+	public function getPikachuName() {
 		return $this->pikachuName;
 	}
 
-	public function getPickachuType() {
+	public function getPikachuType() {
 		return $this->pikachuType;
 	}
 
-	public function getPickachuHitpoints() {
+	public function getPikachuHitpoints() {
 		return $this->pikachuHitpoints;
+	}
+
+	public function getPikachuHealth() {
+		return $this->pikachuHitpoints;
+	}
+
+	public function getPikachuWeakness() {
+		return $this->pikachuWeaknessType;
+	}
+
+	public function getPikachuMultiplier() {
+		return $this->pikachuMultiplier;
+	}
+
+	public function getPikachuNameAttackER() {
+		return $this->pikachuNameAttackER;
+	}
+
+	public function getPikachuNameAttackPP() {
+		return $this->pikachuNameAttackPP;
+	}
+
+	public function getPikachuDamageER() {
+		return $this->pikachuDamageAttackER;
+	}
+
+	public function getPikachuDamagePP() {
+		return $this->pikachuDamageAttackPP;
 	}
 
 	public function getCharmeleonName() {
@@ -28,5 +52,33 @@ class Pokemon {
 
 	public function getCharmeleonHitpoints() {
 		return $this->charmeleonHitpoints;
+	}
+
+	public function getCharmeleonHealth() {
+		return $this->charmeleonHitpoints;
+	}
+
+	public function getCharmeleonWeakness() {
+		return $this->charmeleonWeaknessType;
+	}
+
+	public function getCharmeleonMultiplier() {
+		return $this->charmeleonMultiplier;
+	}
+
+	public function getCharmeleonNameAttackHB() {
+		return $this->charmeleonNameAttackHB;
+	}
+
+	public function getCharmeleonNameAttackF() {
+		return $this->charmeleonNameAttackF;
+	}
+
+	public function getCharmeleonDamageHB() {
+		return $this->charmeleonDamageAttackHB;
+	}
+
+	public function getCharmeleonDamageF() {
+		return $this->charmeleonDamageAttackF;
 	}
 }
